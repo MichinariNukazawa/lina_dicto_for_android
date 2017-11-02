09b0cd614544e43accdc0b513b5bf0858b94168b
Michinari Nukazawa <michinari.nukazawa@gmail.com>
2017-11-03 02:04:12 +0900

    webwiew for simple html

--- app/src/main/java/com/michinari_nukazawa/app/lina_dicto_for_android/MainActivity.java
+++ app/src/main/java/com/michinari_nukazawa/app/lina_dicto_for_android/MainActivity.java
@@ -1,7 +1,10 @@
 package com.michinari_nukazawa.app.lina_dicto_for_android;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView1 = (WebView)findViewById(R.id.webView1);
+        webView1.loadUrl("file:///android_asset/lina_dicto/index.html");
+
+        WebSettings webSettings = webView1.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            webView1.setWebContentsDebuggingEnabled(true);
+        }
     }
 }
