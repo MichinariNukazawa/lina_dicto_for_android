2ff3e167c2670d1217c5a427285783e3af37f911
Michinari Nukazawa <michinari.nukazawa@gmail.com>
2017-11-03 22:56:59 +0900

    handring reshow rotate WebView

--- app/src/main/java/com/michinari_nukazawa/app/lina_dicto_for_android/MainActivity.java
+++ app/src/main/java/com/michinari_nukazawa/app/lina_dicto_for_android/MainActivity.java
@@ -15,8 +15,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         WebView webView1 = (WebView)findViewById(R.id.webView1);
-        webView1.setBackgroundColor(Color.TRANSPARENT);
-        webView1.loadUrl("file:///android_asset/lina_dicto/index.html");
+        if (savedInstanceState == null) {
+            webView1.setBackgroundColor(Color.TRANSPARENT);
+            webView1.loadUrl("file:///android_asset/lina_dicto/index.html");
+        }else{
+            webView1.restoreState(savedInstanceState);
+        }
 
         WebSettings webSettings = webView1.getSettings();
         webSettings.setJavaScriptEnabled(true);
@@ -24,5 +28,21 @@ public class MainActivity extends AppCompatActivity {
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             webView1.setWebContentsDebuggingEnabled(true);
         }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState )
+    {
+        super.onSaveInstanceState(outState);
+        WebView webView1 = (WebView)findViewById(R.id.webView1);
+        webView1.saveState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState)
+    {
+        super.onRestoreInstanceState(savedInstanceState);
+        WebView webView1 = (WebView)findViewById(R.id.webView1);
+        webView1.restoreState(savedInstanceState);
     }
 }
